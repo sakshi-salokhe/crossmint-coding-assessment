8723603b59eb7fe39cc9a01369ae401e5b180969
sakshi-salokhe <sakshisalokhe1@gmail.com>
2024-11-28 14:05:14 -0600

    delete by running file

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from lib.create_megaverse import create_megaverse
 from utils.api_client import ApiClient
+import sys
 
 
-def main():
+def handle_megaverse_creation():
     """
     Main method that runs on initialization
     Retrieves the goal map
@@ -12,8 +13,6 @@ def main():
 
     :return:
     """
-    api_client = ApiClient()
-
     goal_map = api_client.get_goal_map()
 
     if goal_map is not None:
@@ -23,4 +22,26 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    """
+    If the argument for action is delete and contains all arguments, deletes the megaverse object by calling correct endpoint
+    Else creates megaverse
+    """
+    api_client = ApiClient()
+
+    arguments = sys.argv
+
+    action = arguments[1]
+
+    if len(arguments) == 5 and action == 'delete':
+        delete_url_suffix = sys.argv[2]
+        delete_row = sys.argv[3]
+        delete_column = sys.argv[4]
+
+        params = {
+            'row': delete_row,
+            'column': delete_column
+        }
+
+        api_client.handle_delete_api_call(delete_url_suffix, params)
+    else:
+        handle_megaverse_creation()
